0e99cc995971e20eac0a61d1e7fc977847e455a6
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-22 15:51:42 -0300

    Create divisivel-por-cinco.py

---
+++ divisivel-por-cinco.py
@@ -0,0 +1,8 @@
+# Faça um programa em Python que identifica se um número é divisível por 5.
+numero=(int(input("Insira um número inteiro: ")))
+if numero%5 == 0:
+    print("Buzz")
+else:
+    print(numero)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
